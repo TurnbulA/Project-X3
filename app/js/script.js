7e01895a76730669973b38775a9c7d193e0ef032
Xander Turnbull <xanderturnbull2017@gmail.com>
2020-02-11 10:29:01 +0000

    Acted on comments

--- app/js/script.js
+++ app/js/script.js
@@ -6,9 +6,9 @@ document.querySelector(".c-burger-menu").addEventListener("click", () => {
 const icons = [...document.querySelectorAll(".c-icon-container")];
 icons.forEach(icon => {
   icon.addEventListener("click", () => {
-    const projectInfo = document.querySelector(".c-project--info");
+    const projectInfo = document.querySelector(".c-project-info");
     const bodyContainer = document.querySelector(".container");
-    projectInfo.classList.toggle("c-project--info--active");
+    projectInfo.classList.toggle("c-project-info--active");
     bodyContainer.classList.toggle("container--shift-left");
   });
 });
