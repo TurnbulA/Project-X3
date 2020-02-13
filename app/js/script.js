c41749aa736791037adcd63dd7f2892c981fd3d6
Xander Turnbull <xanderturnbull2017@gmail.com>
2020-02-13 13:45:37 +0000

    Acted on comments

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,10 @@
+// Handles Burger menu open/close
 document.querySelector(".c-burger-menu").addEventListener("click", () => {
   const navInner = document.querySelector(".c-burger-menu__inner");
   navInner.classList.toggle("c-burger-menu__inner--active");
 });
 
+// Handles project selection, produces project popup and shifts main body
 const icons = [...document.querySelectorAll(".c-icon-container")];
 icons.forEach(icon => {
   icon.addEventListener("click", () => {
@@ -13,6 +15,7 @@ icons.forEach(icon => {
   });
 });
 
+//Handles opening of add project popup
 document
   .querySelector(".c-body__add-project--icon")
   .addEventListener("click", () => {
@@ -20,6 +23,7 @@ document
     addProject.classList.toggle("c-add-project__popup--active");
   });
 
+//Handles closing of add project popup
 document
   .querySelector(".c-add-project__close--cross")
   .addEventListener("click", () => {
@@ -28,9 +32,12 @@ document
     removeActiveColour();
   });
 
+//Creates an array of project colours
 const newProjectColours = [
   ...document.querySelectorAll(".c-add-project__colour--item")
 ];
+
+//Checks if there is an active colour and removes that class
 const removeActiveColour = () => {
   const activeColours = [...document.querySelectorAll(".isActive")];
   activeColours.forEach(activeColour => {
@@ -38,6 +45,7 @@ const removeActiveColour = () => {
   });
 };
 
+//Adds active colour to 1 item
 newProjectColours.forEach(newProjectColour => {
   newProjectColour.addEventListener("click", () => {
     removeActiveColour();
