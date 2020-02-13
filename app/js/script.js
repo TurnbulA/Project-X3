1b59a5e9f070e20bcb7f5fe1e69c6c732a7bed3f
Xander Turnbull <xanderturnbull2017@gmail.com>
2020-02-13 10:42:39 +0000

    Built frame of new project creation

--- app/js/script.js
+++ app/js/script.js
@@ -12,3 +12,36 @@ icons.forEach(icon => {
     bodyContainer.classList.toggle("c-main--shift-left");
   });
 });
+
+document
+  .querySelector(".c-body__add-project--icon")
+  .addEventListener("click", () => {
+    const addProject = document.querySelector(".c-add-project__popup");
+    addProject.classList.toggle("c-add-project__popup--active");
+  });
+
+document
+  .querySelector(".c-add-project__close--cross")
+  .addEventListener("click", () => {
+    const closeAddProject = document.querySelector(".c-add-project__popup");
+    closeAddProject.classList.toggle("c-add-project__popup--active");
+    removeActiveColour();
+  });
+
+const newProjectColours = [
+  ...document.querySelectorAll(".c-add-project__colour--item")
+];
+const removeActiveColour = () => {
+  const activeColours = [...document.querySelectorAll(".isActive")];
+  activeColours.forEach(activeColour => {
+    activeColour.classList.remove("isActive");
+  });
+};
+
+newProjectColours.forEach(newProjectColour => {
+  newProjectColour.addEventListener("click", () => {
+    removeActiveColour();
+    newProjectColour.classList.toggle("isActive");
+    return newProjectColour;
+  });
+});
