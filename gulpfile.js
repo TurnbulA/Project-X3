b20d0f61dc40bc5ea0ff3261e19590ab18ace873
Xander Turnbull <xanderturnbull2017@gmail.com>
2020-02-25 07:55:57 +0000

    Removed all scss files and corrected/ replaced files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const minify = require("gulp-minify");
+const concat = require("gulp-concat");
 
 gulp.task("compress", () => {
   return gulp
@@ -14,8 +15,15 @@ gulp.task("sass", () => {
     .pipe(sass())
     .pipe(gulp.dest("dist/"));
 });
+gulp.task("concat", () => {
+  return gulp
+    .src("app/js/*.js")
+    .pipe(concat("concatScript.js"))
+    .pipe(gulp.dest("dist/"));
+});
 
 gulp.task("watch", () => {
   gulp.watch("app/scss/*.scss", gulp.series(["sass"]));
   gulp.watch("app/js/*.js", gulp.series(["compress"]));
+  gulp.watch("app/js/*.js", gulp.series(["concat"]));
 });
